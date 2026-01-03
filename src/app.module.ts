6eb0ddb1260feab42a80c7ad24c21e1a7e66fba5
Nadib-Rana <codecrafersnadib@gmail.com>
2026-01-03 23:13:30 +0600

    add user

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { ContextModule } from './common/context/context.module';
 import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
 import { APP_INTERCEPTOR, Reflector } from '@nestjs/core'; // <-- Import Reflector
 import { ResponseStandardizationInterceptor } from './common/interceptors/response-standardization.interceptor';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ContextModule],
+  imports: [ContextModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
