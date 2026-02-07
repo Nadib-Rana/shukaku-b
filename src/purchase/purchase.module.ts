e64c50c86bacbf635c92dd8300eb54182f90ea8a
Nadib-Rana <codecrafersnadib@gmail>
2026-02-08 03:16:43 +0600

    add revenu card

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { PurchaseController } from './purchase.controller';
+import { RevenueCatController } from './revenuecat.controller';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
-  controllers: [PurchaseController],
+  imports: [PrismaModule],
+  controllers: [PurchaseController, RevenueCatController],
   providers: [PurchaseService],
+  exports: [PurchaseService],
 })
 export class PurchaseModule {}
